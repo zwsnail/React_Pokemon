0fef6dc625168fe81a5b59685c752611cb041a8b
coco <coco@192.168.0.106>
2023-02-01 19:04:12 +0800

    chore: add react-helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { PokemonIndex } from "./components";
 import "./App.css";
 import { Footer } from "./components/footer";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Pokemon React App</title>
+        <meta name="description" content="Pokemon App" />
+        <meta name="keywords" content="Pokemon, React, API" />
+      </Helmet>
       <div className="App">
         <PokemonIndex />
       </div>
