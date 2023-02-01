a4a27f86081c70721d7bc3a370a7e640d6ec48f1
coco <coco@192.168.0.106>
2023-02-01 23:54:00 +0800

    style: resize the logo

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <title>Pokemon React App</title>
         <meta name="description" content="Pokemon App" />
         <meta name="keywords" content="Pokemon, React, API" />
-        <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=3.0, minimum-scale=0.86" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Helmet>
       <div className="App">
         <PokemonIndex />
--- src/components/search-panel.jsx
+++ src/components/search-panel.jsx
@@ -5,7 +5,7 @@ export const SearchPokemon = ({ onChangeHandler, totalPokemon }) => {
 
   return (<Wrapper>
     {/* <img src={PokeballImage}></img> */}
-    <Image style={{ marginBottom: "45px" }} width={200} src={logo}></Image>
+    <Image style={{ marginBottom: "45px" }} src={logo}></Image>
     <h1>Search Your Favorite Pokemon</h1>
     <ShorterInput onChange={onChangeHandler} placeholder="Search..." />
     <p style={{ fontSize: 10, marginTop: 10 }}>~ Do you know there are <b>{totalPokemon}</b> pokemons⭐️~</p>
@@ -16,6 +16,21 @@ const Wrapper = styled.div`
   width: 100%;
   padding: 4em;
   background: var(--bg-primary);
+  .ant-image {
+    width: 20rem;
+  }
+
+  @media screen and (max-width: 575px) {
+    .ant-image {
+      width: 50%;
+    }
+    .ant-image-img  {
+      margin-bottom: 15px !important;
+    }
+  }
+
+  
+
 `;
 const ShorterInput = styled(Input)`
   width: 50%;
