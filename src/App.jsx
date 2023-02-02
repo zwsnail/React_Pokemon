2ecb49d9f09d5d220a0dd0566043eea6806616c2
coco <coco@192.168.0.106>
2023-02-02 11:53:48 +0800

    style: add root style

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
         <title>Pokemon React App</title>
         <meta name="description" content="Pokemon App" />
         <meta name="keywords" content="Pokemon, React, API" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       </Helmet>
       <div className="App">
         <PokemonIndex />
